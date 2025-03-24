4709b9fc3a1249abee4836f73ef887ffa14e8312
mateus-aleixo <mateus.aleixo@ubi.pt>
2025-03-24 19:30:59 +0000

    remove duplicate frames

--- app/util.py
+++ app/util.py
@@ -1,11 +1,20 @@
-from functions import extract_video_frames
+from functions import extract_video_frames, remove_duplicate_frames
 import os
 
+
 def main():
+    # Extract frames from all videos in the data/videos directory
     for video in os.listdir("data/videos"):
         if video.endswith(".mp4"):
             video_path = os.path.join("data", "videos", video)
             extract_video_frames(video_path)
+
+    # Remove duplicate frames from all extracted frames directories
+    for file in os.listdir("data/frames"):
+        if os.path.isdir(os.path.join("data", "frames", file)):
+            frames_dir = os.path.join("data", "frames", file)
+            remove_duplicate_frames(frames_dir)
+
 
 if __name__ == "__main__":
     main()
