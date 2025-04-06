418fd2146e301c6abaf7af998a0ab6bbbbd56027
Diogo Marto <diogo.marto@ua.pt>
2025-04-06 19:48:55 +0100

    Update Dockerfile and main.py to adjust command execution and define main function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
 # The script that makes the inference and puts the result in the queue
+
+def main():
+    print("This is the main function of the app.")
+    
+if __name__ == "__main__":
+    main()
