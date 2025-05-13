f9106f27cc36c88821049d35382b6e587eb01858
mateus-aleixo <mateus.aleixo@ubi.pt>
2025-05-13 12:22:35 +0100

    Add path to app/classes in conftest.py for test imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,4 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "../app"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../app/classes"))
